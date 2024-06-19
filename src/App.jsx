b6cb896d47fb60b92e056f2084513d26c8358b9f
amaan <amaan.ali.ug22@nsut.ac.in>
2024-06-19 16:00:33 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h2>Annotsav </h2>
+      {/* import any component you are working on */}
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+
+        "primaryColor-1": "#365e32",
+        "primaryColor-2": "#81a263",
+        "primaryColor-3": "#e7d37f",
+        "primaryColor-4": "#fd9b63",
+        "primaryColor-5": "#4C9A44",
+        "secondaryColor-1": "#6f4e37",
+        "secondaryColor-2": "#a67b5b",
+        "secondaryColor-3": "#fed8b1",
+        "secondaryColor-4": "#ecb176"
+      }
+    },
+  },
+  plugins: [],
+}
