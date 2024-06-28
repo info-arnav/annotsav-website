bb73a475c95d35117c32ae6668fdfd0d0457072e
aniket negi <aniket.negi19104@gmail.com>
2024-06-28 23:53:34 +0530

    rev: font smaller and button center on mobile on Organisation Form

--- src/components/OrganisationForm.jsx
+++ src/components/OrganisationForm.jsx
@@ -18,12 +18,12 @@ function OrganisationForm() {
       <div className="">
         <section className="flex items-center lg:justify-between flex-col lg:flex-row space-y-10 lg:space-y-0 m-24">
           {/* tag line part */}
-          <div className="text-6xl font-bold">
+          <div className="text-3xl lg:text-6xl font-bold">
             <p>Get Annotsav</p> <p>for your</p>{" "}
             <p className="  text-secondaryColor-4">Organisation</p>
           </div>
           {/* form input area  */}
-          <form className="">
+          <form className="flex flex-col items-center lg:items-stretch">
             <input
               type="text"
               id="name"
@@ -104,7 +104,7 @@ function OrganisationForm() {
               cols="33"
               placeholder="MESSAGE"
               className="p-2 border-[.1em] rounded-sm border-black focus:outline-none w-full  resize-none mt-2"></textarea>
-            <motion.button className="bg-primaryColor-5 text-yellow-50  rounded-full w-20 h-8 text-[16px] px-4 relative -right-[80%]   mt-2"
+            <motion.button className="bg-primaryColor-5 text-yellow-50  rounded-full w-20 h-8 text-[16px] px-4 relative right-0 lg:-right-[80%]   mt-2"
               variants={btnVariant}
               whileHover={"hover"}
               whileTap={"tap"}
