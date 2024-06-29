19fab7f65602e69fe7fc892396866ef62082d91e
aniket negi <aniket.negi19104@gmail.com>
2024-06-29 15:36:10 +0530

    fix: contact form headings and button

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,9 +6,9 @@ function ContactForm() {
       <div className="">
         <section className="flex flex-col lg:flex-row items-center lg:justify-between m-24">
           {/* tag line part */}
-          <div className="text-4xl lg:text-6xl font-bold p-20">
+          <div className="text-5xl lg:text-6xl font-bold p-20">
             <p>Join the</p>
-            <p className="text-secondaryColor-4 text-5xl lg:text-7xl">Movement 🪴</p>
+            <p className="text-secondaryColor-4 text-6xl lg:text-7xl">Movement 🪴</p>
           </div>
           {/* form input area  */}
           <form className="flex flex-col items-center lg:items-stretch">
@@ -51,8 +51,7 @@ function ContactForm() {
               cols="33"
               placeholder="Tell us what you want to do for the moment"
               className="p-2 border-[.1em] rounded-sm border-black focus:outline-none w-full  resize-none mt-2"></textarea>
-            <motion.button
-              className="bg-primaryColor-5 text-yellow-50  rounded-full w-20 h-8 text-[16px] px-4 relative right-0 lg:-right-[80%]   mt-2"
+            <motion.button className="bg-primaryColor-5 text-yellow-50 rounded-md lg:rounded-full w-40 lg:w-20 h-8 text-[16px] px-4 relative right-0 lg:-right-[80%] mt-4"
               variants={btnVariant}
               whileHover={"hover"}
               whileTap={"tap"}>
