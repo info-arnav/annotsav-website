b80c935c653a26669b97eab0d5736d0b236eef4f
aniket negi <aniket.negi19104@gmail.com>
2024-06-29 15:34:36 +0530

    fix: volunteer form headings and button

--- src/components/VolunteerForm.jsx
+++ src/components/VolunteerForm.jsx
@@ -8,7 +8,7 @@ function VolunteerForm() {
       <div className="">
         <section className="flex flex-col lg:flex-row lg:justify-between space-y-10 lg:space-y-0 m-24">
           {/* tag line part */}
-          <div className="text-3xl lg:text-6xl font-bold">
+          <div className="text-5xl lg:text-6xl font-bold">
             <p>Share your 💡</p> <p>idea on how</p>
             <p>we can grow</p>
             <p className=" text-secondaryColor-4">Annotsav</p>
@@ -46,7 +46,7 @@ function VolunteerForm() {
               cols="33"
               placeholder="Tell us what you want to do for the moment"
               className="p-2 border-[.1em] rounded-sm border-black focus:outline-none w-full  resize-none mt-2"></textarea>
-            <motion.button className="bg-primaryColor-5 text-yellow-50  rounded-full w-20 h-8 text-[16px] px-4 relative right-0 lg:-right-[80%]   mt-2"
+            <motion.button className="bg-primaryColor-5 text-yellow-50 rounded-md lg:rounded-full w-40 lg:w-20 h-8 text-[16px] px-4 relative right-0 lg:-right-[80%] mt-4"
               variants={btnVariant}
               whileHover={"hover"}
               whileTap={"tap"}
